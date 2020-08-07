e63d3a0ad7804b315f024456c0c44027a1afda48
UwaNDoppio <uwandoppio@gmail.com>
2020-08-07 14:53:23 +0900

    トップページへのリダイレクト処理

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :move_to_index, except: [:index, :show]
 
   def index
   end
@@ -12,6 +13,12 @@ class ItemsController < ApplicationController
   def show
   end
 
-  
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
+
+
 
 end
