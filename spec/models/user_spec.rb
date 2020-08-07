f01f622f5a05bba6e3dca51d989d31ed292c56e9
UwaNDoppio <uwandoppio@gmail.com>
2020-08-07 16:02:04 +0900

    ユーザー情報exampleの整理

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,29 @@
+require 'rails_helper'
+describe User, type:  :model do
+  describe 'ユーザー情報' do
+    it "ニックネームが必須であること" do
+      
+    end
+    it "メールアドレスが必須であること" do
+      
+    end
+    it "メールアドレスが一意性であること" do
+      
+    end
+    it "メールアドレスは＠を含む必要があること" do
+      
+    end
+    it "パスワードが必須であること" do
+      
+    end
+    it "パスワードは6文字以上であること" do
+      
+    end
+    it "パスワードは半角英数字混合であること" do
+      
+    end
+    it "パスワードは確認用を含めて２回入力すること" do
+      
+    end
+  end
+end
