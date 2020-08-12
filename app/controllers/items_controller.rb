6c7e8d381e54a4b8a4a823de41df93cdc6822eaa
UwaNDoppio <uwandoppio@gmail.com>
2020-08-12 14:00:22 +0900

    アクティブハッシュの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,12 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :edit]
 
   def index
     @itmes = Item.order(product_name: "DESC")
   end
 
   def new
+    
   end
 
   def edit
