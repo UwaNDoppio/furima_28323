3c8e7527e1809f21a76f7288f05f1311f80b8023
UwaNDoppio <uwandoppio@gmail.com>
2020-08-25 09:32:35 +0900

    出品時にサインインの促し

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!
   before_action :set_item, only: [:edit, :show,:destroy]
 
   def index
